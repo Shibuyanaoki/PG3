c8bf93164c6fe168373bf2827e9d0dcb57ba11fb
Naoshi2772@icloud.com <Naoshi2772@icloud.com>
2023-10-13 14:25:55 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main() {
+	int Recursive(int h, int j, int m) {
 
-	return 0;
+		if (h <= 0) {
+			//ˆ—‚ª¬Œ÷‚µ‚½ê‡
+			return j;
+		}
 
-}
+		return Recursive(h - 1, j += m, m * 2 - 50);
+
+	}
+
+	int main() {
+		int hour = 4;
+		int money = 100;
+		int total = 0;
+		int normal = 1072;
+
+		int result;
+
+		result = Recursive(hour, total, money);
+
+		printf("3ŠÔ“­‚¢‚½ˆê”Ê’À‹à‘ÌŒn %d F Ä‹A“I‚È’À‹à‘ÌŒn %d", normal * hour, result);
+
+		return 0;
+	}
