a274cc66064672b8a3bef49dbd4d1dae58e0a59f
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2023-10-29 22:50:20 +0900

    02_02

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ int main() {
 
 	MT<int>num1(3, 5);
 	MT<float>num2(1.5f, 2.1f);
+	MT<double>num3(2.5f, 5.1f);
 
 	printf("%d\n", num1.Min());
 	printf("%f\n", num2.Min());
+	printf("%f\n", num3.Min());
 
 	return 0;
 
