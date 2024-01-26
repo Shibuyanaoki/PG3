1bdcb3eea83d0c911fea3df817f335afd897d54a
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-01-26 19:02:13 +0900

    完成した

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <thread>
+
+void Print1() {
+	printf("thread1\n");
+}
+
+void Print2() {
+	printf("thread2\n");
+}
+
+void Print3() {
+	printf("thread3\n");
+}
 
 int main() {
+
+	std::thread th1(Print1);
+	th1.join();
+
+	std::thread th2(Print2);
+	th2.join();
+
+	std::thread th3(Print3);
+	th3.join();
 
 	return 0;
 
