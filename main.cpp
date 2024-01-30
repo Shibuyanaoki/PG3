609913c297806c846223b782a7c44087683ab9f4
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-01-30 15:30:51 +0900

    修正中

--- main.cpp
+++ main.cpp
@@ -2,15 +2,26 @@
 #include <thread>
 
 void Print1() {
+
 	printf("thread1\n");
 }
 
 void Print2() {
+
 	printf("thread2\n");
 }
 
 void Print3() {
+
 	printf("thread3\n");
+}
+
+void Num(int num) {
+
+	for (num; num <= 3; ++num) {
+		printf("thread%d\n",num);
+	}
+
 }
 
 int main() {
@@ -21,10 +32,12 @@ int main() {
 
 	std::thread th3(Print3);
 
+	//std::thread th4(Num,1);
 
 	th1.join();
 	th2.join();
 	th3.join();
+	//th4.join();
 
 	return 0;
 
